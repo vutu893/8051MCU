7b8257877e106b0ca5b706cee0cfa826c6f5c067
vutrinhtu <vutubtt893@gmail.com>
2024-05-23 09:31:35 +0700

    [ADD] add mode led 4

--- Project/dichsangled/main.c
+++ Project/dichsangled/main.c
@@ -7,6 +7,7 @@
 void mode_led_1();
 void mode_led_2();
 void mode_led_3();
+void mode_led_4();
 
 int main()
 {		
@@ -15,7 +16,7 @@ int main()
 		delay_ms(1000);
 		while(1)
 		{
-			mode_led_3();
+			mode_led_4();
 		}
 }
 //che do sang thu nhat
@@ -53,6 +54,28 @@ void mode_led_3()
 	{
 		LED_PORT = 0x80 >> i;
 		delay_ms(500);
-	}
-	
+	}	
 }
+
+//che do sang thu tu
+void mode_led_4()
+{
+	unsigned char i,j;
+	delay_ms(200);
+	for(i = 0; i < 8; i++)
+	{
+		for(j = 0; j < 8 - i;j++)
+		{
+			LED_PORT = LED_PORT|1 << j;
+			delay_ms(250);
+		}
+		//temp = 0x01;
+		LED_PORT = LED_PORT^(LED_PORT >> (i+1));
+		delay_ms(250);
+	}
+	LED_PORT = ~LED_PORT;
+	delay_ms(300);
+} 
+
+//che do sang thu 5
+
