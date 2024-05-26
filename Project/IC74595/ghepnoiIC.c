86c7f29e4191122737569429f44bc537cf5246bd
vutrinhtu <vutubtt893@gmail.com>
2024-05-26 18:22:02 +0700

    [CHANGE] dung thu vien cho IC74595

--- Project/IC74595/ghepnoiIC.c
+++ Project/IC74595/ghepnoiIC.c
@@ -1,33 +1,7 @@
 //bai2: ghep noi nhieu IC
 #include <REGX52.H>
 #include "../../Lib/delay.h"
-
-sbit IC74595_SHCP = P2^0;
-sbit IC74595_DS = P2^1;
-sbit IC74595_STCP = P2^2;
-
-//dung con tro boi vi trong main ta se dung mang va de thay doi gia tri truyen thi phai dung con tro trong ham
-void IC74595_out(unsigned char *p, unsigned char n)
-{
-	unsigned char i, b, j;
-	for(j = 0; j < n;j++)
-	{
-		//lay byte cao nhat truoc
-		b = *(p - j + n -1);
-		
-		for(i = 0; i < 8; i++)
-		{
-			IC74595_DS = b & (0x80 >> i);
-			
-			//tao xung dich du lieu
-			 IC74595_SHCP = 0;
-			 IC74595_SHCP = 1;
-		}
-	}
-	//tao xung xuat du lieu
-	IC74595_STCP = 0;
-	IC74595_STCP = 1;
-}
+#include "../../Lib/IC74595.h"
 
 void main()
 {
