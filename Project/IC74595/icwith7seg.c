95c60ec259daaa3c69d34bae05c8758861546385
vutrinhtu <vutubtt893@gmail.com>
2024-05-26 18:45:24 +0700

    [ADD] dung 1 IC: dung con tro

--- Project/IC74595/icwith7seg.c
+++ Project/IC74595/icwith7seg.c
@@ -1,24 +1,18 @@
 #include <REGX52.H>
 #include  "../../Lib/delay.h"
 #include "../../Lib/IC74595.h"
-
+//so luong phan tu trong buffer la so luong ic dung
+// truong hop dung 1 ic
 unsigned char led7seq[] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90};
 void main()
 {
 	unsigned char i,j;
-	unsigned char buffer[2];
+	unsigned char* buffer;
 	while(1)
 	{	
-		for(j = 0; j < 2; j++)
-		{
-			buffer[j] = led7seq[i];
-		}
 		for(i = 0; i < 10; i++)
 		{
-			for(j = 0; j < 2; j++)
-			{
-				buffer[j] = led7seq[i];
-			}
+			buffer = &led7seq[i];
 			IC74595_out(buffer, 9);
 			delay_ms(1000);
 		}
