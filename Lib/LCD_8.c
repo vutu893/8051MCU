65777e98e88d826be37425c0a8a8c8770c2352be
vutrinhtu <vutubtt893@gmail.com>
2024-05-31 01:14:55 +0700

    [ADD] add description for lib

--- Lib/LCD_8.c
+++ Lib/LCD_8.c
@@ -1,6 +1,38 @@
+
+//xay dung thu vien cho LCD o cach ket noi 8bit voi cac chuc nang sau day:
+//1. TAO HAM KHOI TAO CHO LCD
+//2. TAO HAM LCD COMMAND DE THAO TAC VOI LCD
+//3. TAO HAM GHI KI TU
+//4. TAO HAM GHI CHUOI KI TU
+//5. TAO HAM HIEN THI SO 
+//6. HAM DICH KI TU TREN LCD
+//7. HAM HIEN THI KY TU O VI TRI BAT KY
+//8. HAM HIEN THI CHUOI O VI TRI BAT KY
+//9. HAM TAO KI TU CHO TRUOC TREN LCD CO HIEU UNG HIEN THI
+
+
+///-------------------------------build library lcd connect 8bit-------------------------------------------------
+
+//cac thu vien can include
 #include "delay.h"
 #include "port.h"
 #include "LCD_8.h"
+
+//init lcd
+void lcd_init()
+{
+	lcd_cmd(_LCD_8BIT_1LINE_5x7FONT);
+	delay_ms(5);
+	lcd_cmd(_LCD_8BIT_1LINE_5x7FONT);
+	delay_ms(1);
+	
+	lcd_cmd(_LCD_8BIT_1LINE_5x7FONT);
+	lcd_cmd(_LCD_8BIT_2LINE_5x7FONT);//so dong la 2, font chu la 5x8
+	
+	lcd_cmd(0x01); //xoa noi dung tren lcd
+	lcd_cmd(0x0C); //bat hien thi va xoa con tro
+}
+
 //thiet lap che do cho lcd
 void lcd_cmd(unsigned char cmd)
 {
@@ -17,17 +49,3 @@ void lcd_cmd(unsigned char cmd)
 	}
 }
 
-//init lcd
-void lcd_init()
-{
-	lcd_cmd(_LCD_8BIT_1LINE_5x7FONT);
-	delay_ms(5);
-	lcd_cmd(_LCD_8BIT_1LINE_5x7FONT);
-	delay_ms(1);
-	
-	lcd_cmd(_LCD_8BIT_1LINE_5x7FONT);
-	lcd_cmd(_LCD_8BIT_2LINE_5x7FONT);//so dong la 2, font chu la 5x8
-	
-	lcd_cmd(0x01); //xoa noi dung tren lcd
-	lcd_cmd(0x0C); //bat hien thi va xoa con tro
-}
