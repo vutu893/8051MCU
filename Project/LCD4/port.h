853cead6497099c7b828bcc357aa2b772c7d5a7e
vutrinhtu <vutubtt893@gmail.com>
2024-06-01 01:05:02 +0700

    [ADD] file port for lib led4

---
+++ Project/LCD4/port.h
@@ -0,0 +1,12 @@
+#ifndef _PORT_H_
+#define _PORT_H_
+
+sbit LCD_RS	= P0^0;
+sbit LCD_RW = P0^1; 
+sbit LCD_EN	= P0^2;
+sbit LCD_D4	= P2^4;
+sbit LCD_D5 = P2^5;
+sbit LCD_D6 = P2^6;
+sbit LCD_D7 = P2^7;
+
+#endif
