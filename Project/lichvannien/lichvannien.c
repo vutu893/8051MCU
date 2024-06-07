f0729e1d535d3dee7966d6911477b84b4e525ad6
vutrinhtu <vutubtt893@gmail.com>
2024-06-07 23:28:00 +0700

    [ADD] su dug ngat va ru ngu vdk => tiet kiem nang luong

--- Project/lichvannien/lichvannien.c
+++ Project/lichvannien/lichvannien.c
@@ -5,10 +5,11 @@
 #include "../../Lib/Rtc_Ds1307.h"
 //su dung IC ve thoi gian thuc: ds1307
 //su dung ngat INT1
+//su dung chan SOUT noi voi chan INT1 cua at89c52
 unsigned char* Days[] = {"SUN","MON","TUE", "WED", "THU", "FRI", "SAT"};
 void main()
 {	
-	unsigned char hour, minute, second, mode, Date, month, year, old_second, day;
+	unsigned char hour, minute, second, mode, Date, month, year,  day;
 	
 	Soft_I2c_Init();
 	
@@ -16,12 +17,21 @@ void main()
 	
 	Lcd_Init();
 	
+	//tao dao dong o chan SOUT
+	Ds1307_Write(0x07, 0x10);
+	
+	//Ngat canh xuong
+	IT1 = 1;
+	
+	//Cho phep ngat
+	EX1 = 1;
+	
+	//cho phep ngat toan cuc
+	EA = 1;
 	while(1)
 	{
-		Ds1307_Read_Time(&hour, &minute, &second, &mode);
-		if(old_second != second )
-		{	
-			old_second = second;
+			Ds1307_Read_Time(&hour, &minute, &second, &mode);
+			
 			Lcd_Chr(1,5,hour / 10 + 0x30);
 			Lcd_Chr_Cp(hour % 10 + 0x30);
 			Lcd_Chr_Cp(':');
@@ -43,7 +53,13 @@ void main()
 			Lcd_Out_Cp("20");
 			Lcd_Chr_Cp(year/10 + 0x30);
 			Lcd_Chr_Cp(year%10 + 0x30);
-		}
-		
+			
+			//che do nghi - vdk vao che do nghi=> tiet kiem nang luong
+			PCON |= 0x01;
 	}
+}
+
+//chuong trinh phuc vu ngat: ISR interrup service routine
+void EX1_ISR(void) interrupt 2
+{
 }
