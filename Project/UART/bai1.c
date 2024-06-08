d8ea625922ba397e7a684a06e300e0a2eea29c13
vutrinhtu <vutubtt893@gmail.com>
2024-06-08 17:10:52 +0700

    [ADD] dung mode 0 UART voi 74hc595

--- Project/UART/bai1.c
+++ Project/UART/bai1.c
@@ -1,1 +1,32 @@
-// dung mode  0 de mo rong ngo ra
+// dung mode  0 de mo rong ngo ra voi 74hc595
+#include <REGX52.H>
+#include "../../Lib/delay.h"
+
+void Uart_Mode0_Write(unsigned char b)
+{
+	//du lieu duoc truyen 
+	SBUF = b;
+	
+	while(TI == 0);
+	TI = 0;
+	
+	P3_2 = 0;
+	P3_2 = 1;
+}
+void main()
+{	
+	unsigned char LED = 0x01;
+	unsigned char i;
+	//CHE DO 0
+	SM0 = SM1 = 0;
+	while(1)
+	{
+		for(i = 0; i < 8; i++)
+		{	
+			LED = LED | 1 << i;
+			Uart_Mode0_Write(LED);
+			delay_ms(500);
+		}	
+		Uart_Mode0_Write(0);
+	}
+}
