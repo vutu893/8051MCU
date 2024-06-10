6a2275a40998c7649b1889e567df4ee1debb58b0
vutrinhtu <vutubtt893@gmail.com>
2024-06-11 01:48:47 +0700

    [UPDATE] on/off led delay 1s bang timer0 mode1 voi interrupt

--- Project/Timer/timer_interrupt.c
+++ Project/Timer/timer_interrupt.c
@@ -1,10 +1,40 @@
 #include <REGX52.H>
 
 //ngat voi timer
+//khi overflow thi se co mot interrup_request
+//cau hinh trong thanh ghi IE: ET0 = 1
+sbit LED = P2^1;
+unsigned char cnt = 0;
+void Timer0_delay_1s_ISR(void) interrupt 1
+{
+	TH0 = 0x3C;
+	TL0 = 0xB0;
+	
+	cnt++;
+	if(cnt >= 20)
+	{
+		LED = !LED;
+		cnt = 0;
+	}
+}
 void main()
 {
+	LED = 0;
+	//cau hinh timer 0 mode 1
+	TMOD &= 0xF0;
+	TMOD |= 0x01;
+	
+	TH0 = 0x3C;
+	TL0 = 0xB0;
+	
+	//cai dat ngat
+	ET0 = 1;
+	EA = 1;
+	
+	//chay timer
+	TR0 = 1;
 	while(1)
 	{
-	
+		
 	}
 }
