787f195c2ba5f66ebe879bcfcbab37114698ca08
vutrinhtu <vutubtt893@gmail.com>
2024-07-16 23:35:20 +0700

    thu phat hong ngoai

---
+++ Project/hongngoai/hong ngoai.c
@@ -0,0 +1,18 @@
+#include <REGX52.H>
+#include "delay.h"
+void main()
+{
+	T2CON = 0x00;
+	TMOD = 0x02;
+	
+	RCAP2H = 0xFF;
+	RCAP2L = 0x31;
+	
+	TH2 = 0xFF;
+	TL2 = 0x31;
+	
+	TR2 = 1;
+	
+	while(1)
+	{}
+}
