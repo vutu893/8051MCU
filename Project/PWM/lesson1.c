fac3381f614d9e0f65d1e9d60162ca755089b8f2
vutrinhtu <vutubtt893@gmail.com>
2024-07-22 23:14:34 +0700

    [ADD] su dung ngat timer0 de tao xung PWM bang phan mem

--- Project/PWM/lesson1.c
+++ Project/PWM/lesson1.c
@@ -1,5 +1,7 @@
 #include <REGX52.H>
 
+//tao xung PWM bang phan mem : dung ngat timer 0 de tao xung PWM  tren chan P2.0.Timer 0 hoat dong o che do dinh thoi 16bit
+// do dao dong la 12MHz nen tan so clock cho timer 0 la 1MHz
 void main()
 {
 	
