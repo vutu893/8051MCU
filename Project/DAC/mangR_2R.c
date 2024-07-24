55940e3b233ac4488875f95dbb1cd6b5cdec88e2
vutrinhtu <vutubtt893@gmail.com>
2024-07-24 23:10:52 +0700

    [ADD] dung mang R 2R de chuyen doi tu so sang tuong tu

--- Project/DAC/mangR_2R.c
+++ Project/DAC/mangR_2R.c
@@ -1,10 +1,20 @@
 #include <REGX52.H>
-#include "../../Lib/delay.c"
-
+#include "../../Lib/delay.h"
+unsigned char i = 0;
 void main()
 {
+	P2 = 0;
 	while(1)
 	{
-		
+		for(i = 0; i < 256; i++)
+		{
+			P2 = i;
+			delay_ms(8);
+		}
+		for(i = 255; i >= 0; i--)
+		{
+			P2 = i;
+			delay_ms(8);
+		}
 	}
 }
