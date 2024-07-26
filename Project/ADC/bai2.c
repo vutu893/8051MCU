43ef2047aa360c53ca7e4cacf9873c3a4eed2ea3
vutrinhtu <vutubtt893@gmail.com>
2024-07-26 23:26:49 +0700

    [ADD] add function ADC0804 read data

--- Project/ADC/bai2.c
+++ Project/ADC/bai2.c
@@ -1,7 +1,34 @@
 #include <REGX52.H>
 #include "../../delay.h"
 
+
+unsigned char ADC0804_Read();
+
 void main()
 {
+	while(1)
+	{
 	
+	}
+}
+unsigned char ADC0804_Read()
+{
+	unsigned char kq;
+	
+	//CHON CHIP VA CHIP BAT DAU HOAT DONG
+	ADC0804_CS = 0;
+	
+	//TAO XUNG BAT DAU CHUYEN DOI
+	ADC0804_WR = 0;
+	ADC0804_WR = 1;
+	
+	//DOI CHO DEN KHI CHUYEN DOI XONG
+	while(ADC0804_INTR);
+	
+	//DOC GIA TRI SAU KHI CHUYEN DOI
+	ADC0804_RD = 0;
+	kq = ADC0804_DATA;
+	ADC0804_RD = 1;
+	
+	return kq;
 }
