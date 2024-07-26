54006ca6b410a1c1dfaa8aad35e49b2ed8504745
vutrinhtu <vutubtt893@gmail.com>
2024-07-27 00:16:30 +0700

    [ADD] add file c of adc0804 library

---
+++ Lib/ADC0804.c
@@ -0,0 +1,25 @@
+#include "ADC0804.h"
+#include <REGX52.H>
+#include "port.h"
+
+unsigned char ADC0804_Read()
+{
+	unsigned char kq;
+	
+	//CHON CHIP VA CHIP BAT DAU HOAT DONG
+	ADC0804_CS = 0;
+	
+	//TAO XUNG BAT DAU CHUYEN DOI
+	ADC0804_WR = 0;
+	ADC0804_WR = 1;
+	
+	//DOI CHO DEN KHI CHUYEN DOI XONG
+	while(ADC0804_INTR);
+	
+	//DOC GIA TRI SAU KHI CHUYEN DOI
+	ADC0804_RD = 0;
+	kq = ADC0804_DATA;
+	ADC0804_RD = 1;
+	
+	return kq;
+}
