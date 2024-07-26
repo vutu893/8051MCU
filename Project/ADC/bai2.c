e609834b4c074fddad55065345519c1da81191d2
vutrinhtu <vutubtt893@gmail.com>
2024-07-27 00:14:46 +0700

    [ADD] use library adc0804

--- Project/ADC/bai2.c
+++ Project/ADC/bai2.c
@@ -2,7 +2,7 @@
 #include "../../Lib/delay.h"
 #include "../../Lib/LCD_8.h"
 #include "port.h"
-unsigned char ADC0804_Read();
+#include "../../Lib/ADC0804.h"
 
 void main()
 {	
@@ -30,25 +30,4 @@ void main()
 		
 		delay_ms(500);
 	}
-}
-unsigned char ADC0804_Read()
-{
-	unsigned char kq;
-	
-	//CHON CHIP VA CHIP BAT DAU HOAT DONG
-	ADC0804_CS = 0;
-	
-	//TAO XUNG BAT DAU CHUYEN DOI
-	ADC0804_WR = 0;
-	ADC0804_WR = 1;
-	
-	//DOI CHO DEN KHI CHUYEN DOI XONG
-	while(ADC0804_INTR);
-	
-	//DOC GIA TRI SAU KHI CHUYEN DOI
-	ADC0804_RD = 0;
-	kq = ADC0804_DATA;
-	ADC0804_RD = 1;
-	
-	return kq;
 }
