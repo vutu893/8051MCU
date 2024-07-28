56d58520f083d68ae50359d64910426e7c236572
vutrinhtu <vutubtt893@gmail.com>
2024-07-29 00:23:23 +0700

    [ADD] overview of one wire

--- Project/1wire/bai1.c
+++ Project/1wire/bai1.c
@@ -1,7 +1,10 @@
 #include <REGX52.H>
 
 
-// gioi thieu ve giao tiep 1 wire
+// gioi thieu ve giao tiep 1 wire: serial protocol
+// 1 wire master control one or more wire slave
+// 1 wire slave has 64 bit ID and include: 8-bit family code( quy dinh loai thiet bi) va 8 - bit de kiem tra loi
+// only transmit or recevie in one time
 void main()
 {
 	while(1)
