e0b04cd237468d53a6ab250fb1c9d97d1e97ae99
vutrinhtu <vutubtt893@gmail.com>
2024-07-29 23:04:57 +0700

    [ADD] communication 1 wire with DS18B20

--- Project/1wire/bai1.c
+++ Project/1wire/bai1.c
@@ -5,6 +5,7 @@
 // 1 wire master control one or more wire slave
 // 1 wire slave has 64 bit ID and include: 8-bit family code( quy dinh loai thiet bi) va 8 - bit de kiem tra loi
 // only transmit or recevie in one time
+// su dung 1 wire voi cam bien nhiet do DS18B20
 void main()
 {
 	while(1)
